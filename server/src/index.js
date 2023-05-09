abe1ec5d91f395b87782fbe3420af40bac15f16e
Suresh Pokhrel <ersureshpk@gmail.com>
2023-05-09 05:55:39 +0545

    client setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,22 @@
 const express = require('express')
 // const port = 8080
-//
 
-const mongoose = require('mongoose');
-const userRoute = require('./route/user')
-const dbConnect = require('./connection/dbConnect')
+// const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express()
+
+const userRoute = require('./route/user')
+const dbConnect = require('./connection/dbConnect')
 const addNewUser= require('./controller/user')
 const User = require('./model/user');
-app.use(cors())
-app.use(express.json());
-app.use("/", userRoute);
-
-dbConnect()
- //
-
 require('dotenv').config();
 console.log(process.env.PORT);
 
+app.use(cors())
+app.use(express.json());
+app.use("/", userRoute);
+dbConnect()
+ //
 
 console.log("connected to database")
 app.use(express.json())
